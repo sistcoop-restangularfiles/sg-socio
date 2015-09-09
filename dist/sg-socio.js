55a23f65db213115e94a1960003a3490d616289d
carlos <carlosthe19916@gmail.com>
2015-09-08 22:32:02 -0500

    REST OBJECT

--- dist/sg-socio.js
+++ dist/sg-socio.js
@@ -30,60 +30,150 @@
         });
     }]);
 
-    module.factory('SGSocio', ['SocioRestangular',  function(SocioRestangular) {
+    var RestObject = function (path, restangular, extendMethods) {
+        var modelMethods = {
 
-        var url = 'socios';
-        var urlCount = 'countryCodes/count';
-
-        var modelMethos = {
-            $new: function(id){
-                return angular.extend({id: id}, modelMethos);
-            },
-            $build: function(){
-                return angular.extend({independent: false}, modelMethos, {$save: function(){
-                    return SocioRestangular.all(url).post(this);
-                }});
-            },
-            $save: function() {
-                return SocioRestangular.one(urlAlpha3Code, this.alpha3Code).customPUT(SocioRestangular.copy(this),'',{},{});
+            /**
+             * Retorna url*/
+            $getModelMethods: function () {
+                return modelMethods;
             },
 
-            $find: function(id){
-                return SocioRestangular.one(url, id).get();
+            /**
+             * Retorna url*/
+            $getBasePath: function () {
+                return path;
             },
-            $search: function(queryParams){
-                return SocioRestangular.all(url).getList(queryParams);
+            /**
+             * Retorna la url completa del objeto*/
+            $getAbsoluteUrl: function () {
+                return restangular.one(path, this.id).getRestangularUrl();
             },
-            $findByAlpha2code: function(alpha2Code){
-                return SocioRestangular.one(urlAlpha2Code, alpha2Code).get();
-            },
-            $findByAlpha3code: function(alpha3Code){
-                return SocioRestangular.one(urlAlpha3Code, alpha3Code).get();
-            },
-            $findByNumericCode: function(numericCode){
-                return SocioRestangular.one(urlNumericCode, numericCode).get();
-            },
-
-            $count: function(){
-                return SocioRestangular.one(urlCount).get();
+            /**
+             * Concatena la url de subresource con la url base y la retorna*/
+            $concatSubResourcePath: function (subResourcePath) {
+                return this.$getBasePath() + '/' + this.id + '/' + subResourcePath;
             },
 
-            $disable: function(){
-                return SocioRestangular.all(url+'/'+this.id+'/disable').post();
+
+            $new: function (id) {
+                return angular.extend({id: id}, modelMethods);
             },
-            $remove: function(id){
-                return SocioRestangular.one(urlAlpha3Code, id).remove();
+            $build: function () {
+                return angular.extend({id: undefined}, modelMethods, {
+                    $save: function () {
+                        return restangular.all(path).post(this);
+                    }
+                });
+            },
+
+            $search: function (queryParams) {
+                return restangular.all(path).customGET('search', queryParams);
+            },
+            $getAll: function (queryParams) {
+                return restangular.all(path).getList(queryParams);
+            },
+
+            $find: function (id) {
+                return restangular.one(path, id).get();
+            },
+            $save: function () {
+                return restangular.one(path, this.id).customPUT(restangular.copy(this), '', {}, {});
+            },
+            $saveSent: function (obj) {
+                return restangular.all(path).post(obj);
+            },
+
+            $enable: function () {
+                return restangular.one(path, this.id).all('enable').post();
+            },
+            $disable: function () {
+                return restangular.one(path, this.id).all('disable').post();
+            },
+            $remove: function () {
+                return restangular.one(path, this.id).remove();
             }
         };
 
-        SocioRestangular.extendModel(url, function(obj) {
-            return angular.extend(obj, modelMethos);
-        });
-        SocioRestangular.extendModel(urlCount, function(obj) {
-            return angular.extend(obj, modelMethos);
+        modelMethods = angular.extend(modelMethods, extendMethods);
+
+        function extendObject(obj, modelMethods){
+            angular.extend(obj, modelMethods);
+        }
+
+        function extendArray(obj, modelMethods){
+            angular.forEach(obj, function (row) {
+                if (angular.isObject(row)) {
+                    if (!angular.isArray(row)) {
+                        extendObject(row, modelMethods);
+                    }
+                }
+            });
+        }
+
+        function automaticExtend(obj, modelMethods){
+            if (angular.isDefined(obj)) {
+                if (angular.isObject(obj)) {
+                    if (angular.isArray(obj)) {
+                        extendArray(obj, modelMethods);
+                    } else {
+                        if (angular.isDefined(obj.items) && angular.isArray(obj.items)) {
+                            extendArray(obj.items, modelMethods);
+                        } else {
+                            extendObject(obj, modelMethods)
+                        }
+                    }
+                }
+            }
+        }
+
+        restangular.extendModel(path, function (obj) {
+            automaticExtend(obj);
+            return obj;
         });
 
-        return modelMethos;
+        restangular.extendCollection(path, function (collection) {
+            automaticExtend(collection);
+            return collection;
+        });
+
+        return modelMethods;
+    };
+
+    module.factory('SGSocio', ['SocioRestangular',  function(SocioRestangular) {
+
+        var extendMethod = {};
+
+        var socioResource = RestObject('socios', SocioRestangular, extendMethod);
+
+        /**
+         * Cuentas personales*
+         * */
+        socioResource.SGCuentaPersonal = function () {
+            var extendMethod = {};
+
+            var ctaPersonalSubResource = RestObject(this.$concatSubResourcePath('cuentasPersonales'), SocioRestangular, extendMethod);
+
+            ctaPersonalSubResource.SGTitular = function () {
+                var extendMethod = {};
+                var ctaPersonalSubResource = RestObject(this.$concatSubResourcePath('titulares'), SocioRestangular, extendMethod);
+                return ctaPersonalSubResource;
+            };
+            ctaPersonalSubResource.SGAutorizado = function () {
+                var extendMethod = {};
+                var ctaPersonalSubResource = RestObject(this.$concatSubResourcePath('autorizados'), SocioRestangular, extendMethod);
+                return ctaPersonalSubResource;
+            };
+            ctaPersonalSubResource.SGBeneficiario = function () {
+                var extendMethod = {};
+                var ctaPersonalSubResource = RestObject(this.$concatSubResourcePath('beneficiarios'), SocioRestangular, extendMethod);
+                return ctaPersonalSubResource;
+            };
+
+            return ctaPersonalSubResource;
+        };
+
+        return socioResource;
 
     }]);
 
